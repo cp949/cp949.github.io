2464c361fd17f88f2e76a9077f5f3e5c6342493c
jjfive <softdada@gmail.com>
2021-11-28 08:12:58 +0900

    minor fix

--- root/src/layout/doc/DocLayout.tsx
+++ root/src/layout/doc/DocLayout.tsx
@@ -32,7 +32,7 @@ export default function Layout(props: Props) {
                 <meta property="og:title" content={meta.title} />
                 <meta property="og:image" content={meta.cardImage} />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@ktaicoder" />
+                <meta name="twitter:site" content="@cp949" />
                 <meta name="twitter:title" content={meta.title} />
                 <meta name="twitter:description" content={meta.description} />
                 <meta name="twitter:image" content={meta.cardImage} /> */}
